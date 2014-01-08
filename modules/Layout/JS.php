041cec1bc3827a8711c23e37f7cbcd40004bd08a
jeyroik <jeyroik@yahoo.com>
2014-01-08 13:27:25 +0300

    Create JS.php

---
+++ modules/Layout/JS.php
@@ -0,0 +1,31 @@
+<?php
+
+trait Layout_JS
+{
+    public $js = '';
+
+    /**
+     * @param string $jsPath
+     */
+    public function addJs($jsPath = '')
+    {
+        $this->js .=
+            "\n\t" . '<script type="text/javascript" src="/js/' . $jsPath . '.js"></script>';
+    }
+
+    /**
+     * Set general JS
+     * @event 'pre dispatch'
+     */
+    public function setGeneralJs()
+    {
+        if(isset($this->onLoad['layout']['js']))
+        {
+            if(!empty($this->onLoad['layout']['js']))
+            {
+                foreach($this->onLoad['layout']['js'] as $js)
+                    $this->addJs($js);
+            }
+        }
+    }
+}
