b30d866df38f7ccc31c26ccbd9763f78f663050b
jeyroik <jeyroik@yahoo.com>
2014-01-08 13:26:21 +0300

    Create Dispatcher.php

---
+++ modules/Dispatcher.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class Dispatcher
+ */
+trait Dispatcher
+{
+    /**
+     * @param string $path
+     * @event 'on load'
+     */
+    public function dispatch($path = '')
+    {
+        $path = $this->rise('pre dispatch', $path)->getEventResult('pre dispatch');
+
+        preg_match_all('/\w+/i', $path, $request);
+
+        $request = isset($request[0]) ? $request[0] : [];// нашли чего-нибудь
+
+        $controller = (isset($request[0]) && $request[0]) ? $request[0] : 'index';
+        $action     = (isset($request[1]) && $request[1]) ? $request[1] : 'index';
+
+        $this->rise($controller . '/' . $action, $request)
+             ->rise('post dispatch',
+                    [
+                        'view' => $controller . '/' . $action,
+                        'data' => $this->getEventResult($controller . '/' . $action)
+                    ]);
+    }
+}
