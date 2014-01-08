e1e43cc398cdd964166f2ae7d47d47e3998eab25
jeyroik <jeyroik@yahoo.com>
2014-01-08 13:25:32 +0300

    Create View.php

---
+++ modules/View.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Class View
+ */
+trait View
+{
+    /**
+     * @var string
+     */
+    public $view = '';
+
+    /**
+     * @param array $request
+     * @return string
+     */
+    public function constructView($request = [])
+    {
+        if(isset($request['view']))
+        {
+            $viewPath = __DIR__ . '/'
+                . $this->onLoad['paths']['views']
+                . $request['view'] . '.' . $this->onLoad['view']['extension'];
+
+            if(file_exists($viewPath))
+                $this->view = $viewPath;
+            else
+            {
+                $this->addError('View is not found', $viewPath);
+                $this->view = false;
+            }
+        }
+        else
+        {
+            $this->addError('Missed view in the request', $request);
+            $this->view = false;
+        }
+
+        return $this->view;
+    }
+}
